42326f37e5ccdb509add11420c219db6e01ccf29
Abdullah Alansari <ahimta@gmail.com>
2016-12-08 14:26:03 +0300

    Allow non-logged users to reserve donations by logging them in and then reserving the donation for them

--- src/app/components/DonationManagementToolbar.tsx
+++ src/app/components/DonationManagementToolbar.tsx
@@ -4,6 +4,7 @@ import * as React from 'react';
 import {Button, ButtonGroup, DropdownButton, MenuItem} from 'react-bootstrap';
 import {hashHistory} from 'react-router';
 
+import * as auth from '../auth';
 import * as database from '../database';
 
 interface IDonationManagementToolbarProps {
@@ -42,9 +43,15 @@ export default class DonationManagementToolbar extends React.Component<IDonation
       <section>
         <ButtonGroup className={this.getReserveClass(currentId, reservation.reserverId, reservation.deliveredOrReceived)}>
           <Button bsStyle='danger' onClick={deleteDonation.bind(null, donationId)} disabled={currentUserId !== donorId}>حذف</Button>
-          <DropdownButton bsStyle='success' dir='rtl' id='reserveDonationButton' title={this.getReserveTitle(reservation.reserverId)} disabled={!!reservation.reserverId || !!!currentId} dropup pullRight>
-            <MenuItem className={currentRole === 'charity' ? 'hidden text-right' : 'text-right'} onClick={this.reserveDonation.bind(this, donationId, 'receiving', currentId, onUpdate)}>لاستقبال التبرع</MenuItem>
-            <MenuItem className='text-right' onClick={this.reserveDonation.bind(this, donationId, 'delivery', currentId, onUpdate)}>لتوصيل التبرع</MenuItem>
+          <DropdownButton bsStyle='success' dir='rtl' id='reserveDonationButton' title={this.getReserveTitle(currentRole, reservation.reserverId)} disabled={!!reservation.reserverId} dropup pullRight>
+            <MenuItem className={currentRole === 'charity' ? 'hidden text-right' : 'text-right'}
+              onClick={this.reserveDonation.bind(this, donationId, 'receiving', currentId, onUpdate)}>
+              لاستقبال التبرع
+            </MenuItem>
+            <MenuItem className='text-right'
+              onClick={this.reserveDonation.bind(this, donationId, 'delivery', currentId, onUpdate)}>
+              لتوصيل التبرع
+            </MenuItem>
           </DropdownButton>
         </ButtonGroup>
         <ButtonGroup className={this.getCancelClass(currentId, reservation.reserverId, reservation.deliveredOrReceived)}>
@@ -65,7 +72,15 @@ export default class DonationManagementToolbar extends React.Component<IDonation
   }
 
   private reserveDonation(donationId: string, reservationType: string, currentId: string, onUpdate: Function) {
-    database.reserveDonation(donationId, reservationType, currentId).then(onUpdate);
+    const helper = (userOrCharityId) => { database.reserveDonation(donationId, reservationType, userOrCharityId).then(onUpdate); };
+
+    if (currentId) {
+      helper(currentId);
+    } else {
+      auth.login().then((user) => {
+        helper(user.uid);
+      });
+    }
   }
 
   private getCancelTitle(reservationType: string) {
@@ -76,11 +91,15 @@ export default class DonationManagementToolbar extends React.Component<IDonation
     }
   }
 
-  private getReserveTitle(reserverId: string) {
+  private getReserveTitle(currentRole: string, reserverId: string) {
     if (reserverId) {
       return 'محجوز';
     } else {
-      return 'حجز';
+      if (currentRole) {
+        return 'حجز';
+      } else {
+        return 'تسجيل دخول و حجز';
+      }
     }
   }
 
