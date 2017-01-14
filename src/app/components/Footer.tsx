8a85f5df8cae6653a5bb70fef6e4e9c78c0fc686
Abdullah Alansari <ahimta@gmail.com>
2017-01-14 11:06:36 +0300

    Update footer
    Add Roundicon copyrights and update year to 2017

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -6,14 +6,20 @@ import ReactGA from 'react-ga';
 export default function Footer() {
   return (
     <footer>
-      <a href='https://sa.linkedin.com/in/ahimta' target='_blank' rel='noopener'
-        onClick={() => ReactGA.event({category: 'Footer', action: 'Clicking', label: 'LinkedIn URL'})}>
-        <p className='text-center lead' dir='rtl'>
+      <p className='text-center' dir='rtl'>
+        بعض الأيقونات المستخدمة من تصميم&nbsp;
+          <a href='http://www.flaticon.com/authors/roundicons' target='_blank' rel='noopener'>Roundicons</a>&nbsp;
+          تحت رخصة&nbsp;
+          <a href='CC 3.0 BY' target='_blank' rel='noopener'>CC 3.0 BY</a>
+      </p>
+      <p className='text-center' dir='rtl'>
+        <a href='https://sa.linkedin.com/in/ahimta' target='_blank' rel='noopener'
+          onClick={() => ReactGA.event({ category: 'Footer', action: 'Clicking', label: 'LinkedIn URL' })}>
           ©
-          2016
+          2017
           عبدالله الأنصاري
-        </p>
-      </a>
+        </a>
+      </p>
     </footer>
   );
 }
