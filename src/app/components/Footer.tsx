5cef97cd06644640c0eb6a899e69791be8ec797f
Abdullah Alansari <ahimta@gmail.com>
2017-01-29 17:33:08 +0300

    Small fixes for footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -8,14 +8,14 @@ export default function Footer() {
     <footer>
       <p className='text-center' dir='rtl'>
         بعض الأيقونات المستخدمة من تصميم&nbsp;
-          <a href='http://www.flaticon.com/authors/roundicons' target='_blank' rel='noopener'>Roundicons</a>&nbsp;
+          <a href='https://www.flaticon.com/authors/roundicons' target='_blank' rel='noopener'>Roundicons</a>&nbsp;
           تحت رخصة&nbsp;
           <a href='CC 3.0 BY' target='_blank' rel='noopener'>CC 3.0 BY</a>
       </p>
       <p className='text-center' dir='rtl'>
-        <a href='https://sa.linkedin.com/in/ahimta' target='_blank' rel='noopener'
+        <a href='https://sa.linkedin.com/in/ahimta' target='_blank'
           onClick={() => ReactGA.event({ category: 'Footer', action: 'Clicking', label: 'LinkedIn URL' })}>
-          ©
+          &copy;
           2017
           عبدالله الأنصاري
         </a>
